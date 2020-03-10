ad0a015ac90babf1717ca526a61b4b36434b72f9
DONNA Gamboa <A01207448@ad.bcit.ca>
2020-03-10 13:42:27 -0700

    Create single-project.php

---
+++ single-project.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying single posts of the Project CPT
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Highlights
+ */
+
+get_header();
+?>
+    <main id="main" class="site-main">
+        TEST HI!
+    </main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
